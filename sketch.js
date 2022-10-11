13f5ce1cff707df1fb99434a6feb6a946d818ef6
Júlia Vieira <112638042+Lia-Moon@users.noreply.github.com>
2022-10-11 11:51:02 -0300

    sketch.js
    Jogo finalizado, criação de menu para escolher entre jogar contra o PC ou multiplayer local, adicionada chance de errar ao oponente PC, etc.
    Link para jogar: https://editor.p5js.org/Lia_Moon/full/XgcaJM4g1 .

--- sketch.js
+++ sketch.js
@@ -1,4 +1,4 @@
-//variáveis bolinh
+//variáveis bolinha
 let xBolinha = 300;
 let yBolinha = 200;
 let diametroBolinha = 18;
@@ -33,6 +33,14 @@ let raquetada;
 let somPonto;
 let trilha;
 
+//botões
+let botaoLocal;
+let botaoAI;
+let botaoRetorno;
+let cliqueBotaoLocal = 0;
+let cliqueBotaoAI = 0;
+let cliqueBotaoRetorno = 0;
+
 function preload(){
   raquetada = loadSound("raquetada.mp3");
   somPonto = loadSound("ponto.mp3");
@@ -42,34 +50,109 @@ function preload(){
 function setup() {
   createCanvas(600, 400);
   trilha.loop();
+  
+  botoes();
 }
 
 /////////////////////////////////////////////////////////////////////////////////////////
 
 function draw() {
   background(0);
-  desenhaBolinha();
   
-  movimentaBolinha();
-  verificaColisaoBordas();
   
-  desenhaRaquete(xMinhaRaquete, yMinhaRaquete);
-  movimentaMinhaRaquete();
+  if(cliqueBotaoLocal == 0 && cliqueBotaoAI == 0){
+    meusPontos = 0;
+    pontosOp = 0;
+    textoBoasVindas();
+    botaoLocal.draw();
+    botaoAI.draw(); 
+  } else if(cliqueBotaoLocal >= 1){
+     iniciaJogoContraAmigo();
+  } else if(cliqueBotaoAI >= 1){
+     iniciaJogoContraAI();
+  }
   
-  //verificaColisaoRaquete();
-  verificaColisaoRaquete(xMinhaRaquete, yMinhaRaquete);
-  verificaColisaoRaquete(xRaqueteOp, yRaqueteOp);
-  
-  desenhaRaquete(xRaqueteOp, yRaqueteOp);
-  movimentaRaqueteAI();
-  //movimentaRaqueteOp();
-  
-  mostraPontos();
-  calculaPontos();
-  
+  ganhaPlayer();
+
 }
 
 /////////////////////////////////////////////////////////////////////////////////////////
+
+function textoBoasVindas(){
+  textAlign(CENTER);
+  textSize(18);
+  //text_font: 'Courier New';
+  fill(255); //preenchendo com a cor branca o texto
+  text('Bem-vindo(a) ao jogo de Ping Pong', 300, 40);
+  textSize(14);
+  text('Criado por Júlia Vieira', width/2, 355);
+  text('Projeto de curso Alura', width/2, 375);
+}
+
+function botoes(){
+
+  botaoLocal = new Button({
+    x: width/2, y: height/2,
+    width: 170, height: 50,
+    align_x: 0, align_y: 2,
+    content: 'Jogo local',
+    on_press() {
+      cliqueBotaoLocal++;
+    }
+  });
+  
+  botaoAI = new Button({
+    x: width/2, y: height/2,
+    width: 170, height: 50,
+    align_x: 0, align_y: -3,
+    content: 'Jogue contra o PC',
+    on_press() {
+      cliqueBotaoAI++;
+    }
+  });
+  
+  botaoRetorno = new Button({
+    x: width/2, y: height/2,
+    width: 250, height: 50,
+    align_x: 0, align_y: 0,
+    content: 'Retornar para tela inicial',
+    on_press() {
+      cliqueBotaoRetorno++;
+      cliqueBotaoLocal = 0;
+      cliqueBotaoAI = 0;
+    }
+  });
+}
+
+function iniciaJogoContraAI(){
+  desenhaBolinha();  
+  movimentaBolinha();
+  verificaColisaoBordas();  
+  desenhaRaquete(xMinhaRaquete, yMinhaRaquete);
+  movimentaMinhaRaquete();  
+  verificaColisaoRaquete(xMinhaRaquete, yMinhaRaquete);
+  verificaColisaoRaquete(xRaqueteOp, yRaqueteOp);  
+  desenhaRaquete(xRaqueteOp, yRaqueteOp);
+  movimentaRaqueteAI(); 
+  mostraPontos();
+  calculaPontos();  
+  bolinhaNaoFicaPresa();
+}
+
+function iniciaJogoContraAmigo(){
+  desenhaBolinha();  
+  movimentaBolinha();
+  verificaColisaoBordas();  
+  desenhaRaquete(xMinhaRaquete, yMinhaRaquete);
+  movimentaMinhaRaquete();  
+  verificaColisaoRaquete(xMinhaRaquete, yMinhaRaquete);
+  verificaColisaoRaquete(xRaqueteOp, yRaqueteOp);  
+  desenhaRaquete(xRaqueteOp, yRaqueteOp);
+  movimentaRaqueteOp();  
+  mostraPontos();
+  calculaPontos();  
+  bolinhaNaoFicaPresa();
+}
 
 function desenhaBolinha(){
   circle(xBolinha, yBolinha, diametroBolinha);
@@ -78,6 +161,7 @@ function desenhaBolinha(){
 function movimentaBolinha(){
   xBolinha += velocidadeXBolinha;
   yBolinha += velocidadeYBolinha;
+  
 }
 
 function verificaColisaoBordas(){
@@ -110,6 +194,7 @@ function movimentaRaqueteAI(){
   yRaqueteOp += velocidadeYOp + chanceDeErrar;
   
   calculaChanceDeErrar();
+
 }
 
 function movimentaRaqueteOp(){
@@ -122,25 +207,16 @@ function movimentaRaqueteOp(){
 }
 
 function calculaChanceDeErrar(){
-  if(pontosOp >= meusPontos){
-    chanceDeErrar += 1;
-    if(chanceDeErrar >= 39){
-      chancedeErrar = 40;
+  if (pontosOp >= meusPontos) {
+    chanceDeErrar += 1
+    if (chanceDeErrar >= 39){
+    chanceDeErrar = 40
     }
-  } else{
-    chanceDeErrar -= 1;
-    if(chanceDeErrar <= -1){
-      chanceDeErrar = 0;
+  } else {
+    chanceDeErrar -= 1
+    if (chanceDeErrar <= 35){
+    chanceDeErrar = 35
     }
-  }
-}
-
-function verificaColisaoRaquete(){
-  if((xBolinha - raio) < (xMinhaRaquete + raqueteComprimento) &&
-     (yBolinha - raio) < (yMinhaRaquete + raqueteAltura) &&
-     (yBolinha + raio) > (yMinhaRaquete)){
-    raquetada.play();
-    velocidadeXBolinha *= -1;
   }
 }
 
@@ -158,11 +234,11 @@ function mostraPontos(){
   textAlign(CENTER);
   textSize(16);
   fill(color(255,105,180));
-  rect(150, 10, 40, 20);
+  rect(150, 14, 40, 20);
   fill(255); //preenchendo com a cor branca o texto
   text(meusPontos, 170, 26);
   fill(color(255,105,180));
-  rect(450, 10, 40, 20);
+  rect(450, 14, 40, 20);
   fill(255); //preenchendo com a cor branca o texto
   text(pontosOp, 470, 26);
 }
@@ -179,9 +255,30 @@ function calculaPontos(){
   }
 }
 
+function bolinhaNaoFicaPresa(){
+  if (xBolinha - raio < 0){
+    xBolinha = 23;
+  }
+  if (xBolinha + raio > 600){
+    xBolinha = 577;
+  }
+}
 
-
-
-
-
+function ganhaPlayer(){
+  if(pontosOp >= 5){
+    botaoRetorno.draw();
+    fill(255);
+    textAlign(CENTER);
+    textSize(20);
+    text('O player dois acaba de ganhar o jogo.', 300, 110);
+    text('Parabéns!!', 300, 145);
+  } else if (meusPontos >= 5){
+    botaoRetorno.draw();
+    fill(255);
+    textAlign(CENTER);
+    textSize(20);
+    text('O player um acaba de ganhar o jogo.', 300, 110);
+    text('Parabéns!!', 300, 145);
+  }
+}
 
